9a2e7f0d3620785ad1faa98fa1d7fda0d0e31916
Vee-Guz <vero.guzman48612@gmail.com>
2021-07-20 21:52:25 -0700

    Fixed issues with script

--- Windows/create-users-and-groups.ps1
+++ Windows/create-users-and-groups.ps1
@@ -9,18 +9,18 @@ New-ADGroup -Name "Sales" -DisplayName "Sales" -GroupScope Global
 New-ADGroup -Name "Security" -DisplayName "Security" -GroupScope Global
 
 #create users - WandaVision characters
-New-ADUser -Name "Wanda Maximoff" -PasswordNotRequired True
-New-ADUser -Name "Vision" -PasswordNotRequired True
-New-ADUser -Name "Monica Rambeau" -PasswordNotRequired True
-New-ADUser -Name "Agatha Harkness" -PasswordNotRequired True
-New-ADUser -Name "Darcy Lewis" -PasswordNotRequired True
-New-ADUser -Name "Jimmy Woo" -PasswordNotRequired True
+New-ADUser -Name "Wanda Maximoff" -PasswordNotRequired $True
+New-ADUser -Name "Vision" -PasswordNotRequired $True
+New-ADUser -Name "Monica Rambeau" -PasswordNotRequired $True
+New-ADUser -Name "Agatha Harkness" -PasswordNotRequired $True
+New-ADUser -Name "Darcy Lewis" -PasswordNotRequired $True
+New-ADUser -Name "Jimmy Woo" -PasswordNotRequired $True
 
 #assign users to groups
-Add-ADGroupMember -Identity Human Resources -Members Wanda Maximoff
-Add-ADGroupMember -Identity Information Technology -Members Vision
-Add-ADGroupMember -Identity Finance -Members Monica Rambeau
-Add-ADGroupMember -Identity Marketing -Members Agatha Harkness
-Add-ADGroupMember -Identity Sales -Members Darcy Lewis
-Add-ADGroupMember -Identity Security -Members Jimmy Woo
+Add-ADGroupMember -Identity "Human Resources" -Members "Wanda Maximoff"
+Add-ADGroupMember -Identity "Information Technology" -Members "Vision"
+Add-ADGroupMember -Identity "Finance" -Members "Monica Rambeau"
+Add-ADGroupMember -Identity "Marketing" -Members "Agatha Harkness"
+Add-ADGroupMember -Identity "Sales" -Members "Darcy Lewis"
+Add-ADGroupMember -Identity "Security" -Members "Jimmy Woo"
 
