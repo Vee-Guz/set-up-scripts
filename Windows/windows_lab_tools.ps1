373ea4c713596f528b8677d8c9eec2bcb9c169c3
Vee-Guz <vero.guzman48612@gmail.com>
2021-08-28 14:34:09 -0700

    Windows script tested and revised to install tools

--- Windows/windows_lab_tools.ps1
+++ Windows/windows_lab_tools.ps1
@@ -1,13 +1,12 @@
 #assesment Aug. 28, 2021
 
 #install IIS
-Enable-WindowsOptionalFeature -Online -FeatureName ISS-WebServerRole
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
 
-#install DNS
-InstallDns:$true
-
-#install ActiveDirectory
-Install-ADDForest -CreateDnsDelegation:$false -DomainName "vero.com" -DomainNetbiosName "VERO" -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true
+#install ActiveDirectory and DNS
+get-windowsfeature
+install-windowsfeature AD-Domain-Services
+Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath “C:\Windows\NTDS” -DomainMode “Win2012R2” -DomainName “vero.com” -DomainNetbiosName “VERO” -ForestMode “Win2012R2” -InstallDns:$true -LogPath “C:\Windows\NTDS” -NoRebootOnCompletion:$false -SysvolPath “C:\Windows\SYSVOL” -Force:$true
 
 #install chocolately
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
