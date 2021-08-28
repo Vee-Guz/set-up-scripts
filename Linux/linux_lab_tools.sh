19444b090b4b1bba48e688adb16aa30c292a85c8
Vee-Guz <vero.guzman48612@gmail.com>
2021-08-28 12:57:22 -0700

    Script to install tools on Linux

---
+++ Linux/linux_lab_tools.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#upgrade and update machine
+sudo apt-get update
+sudo apt-get upagrade
+
+#install tools
+#install NMAP
+sudo apt install nmap
+
+#install Apache
+sudo apt install apache2
+
+#install Medusa
+sudo apt install medusa
+
+#install John the Ripper
+sudo apt-get install john -y
+
+#install MySQL
+sudo apt instal mysql-server
+
+#adding existing database to MySQL
+
